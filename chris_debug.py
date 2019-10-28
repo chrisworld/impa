afc15d48f75f14e9dc743c9280eb92a98397b604
Chris <christian.walter@outlook.at>
2019-10-28 14:03:55 +0100

    primitive methods, but still buggy

--- chris_debug.py
+++ chris_debug.py
@@ -27,24 +27,67 @@ def compute_psnr(img1, img2):
 
 def compute_mean(image, filter_size):
     """
-      For each pixel in the image image consider a window of size
+      For each pixel in the image consider a window of size
       filter_size x filter_size around the pixel and compute the mean
       of this window.
 
       @return: image containing the mean for each pixel
     """
-    return
+    print("mean: ", image.shape)
+
+    # init to zeros
+    mu = np.zeros(image.shape)
+
+    # run through each pixel
+    for row in range(image.shape[0]):
+
+      # skip edges
+      if (row - filter_size) < 0 or (row + filter_size) > image.shape[0]:
+        continue
+
+      for col in range(image.shape[1]):
+
+        # skip edges
+        if (col - filter_size) < 0 or (col + filter_size) > image.shape[1]:
+          continue
+
+        # the pixel means
+        mu[row, col] = np.mean(image[row - filter_size : row + filter_size + 1, col - filter_size : col + filter_size + 1])
+
+    return mu
 
 
 def compute_variance(image, filter_size):
     """
-      For each pixel in the image image consider a window of size
+      For each pixel in the image consider a window of size
       filter_size x filter_size around the pixel and compute the variance
       of this window.
 
-      @return: image containing the variance (\sigma^2) for each pixel
+      @return: image containing the variance (sigma^2) for each pixel
     """
-    return
+    print("var: ", image.shape)
+
+    # init to zeros
+    var = np.zeros(image.shape)
+
+    # run through each pixel
+    for row in range(image.shape[0]):
+
+      # skip edges
+      if (row - filter_size) < 0 or (row + filter_size) > image.shape[0]:
+        continue
+
+      for col in range(image.shape[1]):
+
+        # skip edges
+        if (col - filter_size) < 0 or (col + filter_size) > image.shape[1]:
+          continue
+
+        # the pixel variances
+        var[row, col] = np.var(image[row - filter_size : row + filter_size + 1, col - filter_size : col + filter_size + 1])
+
+
+    return var
 
 
 def compute_a(F, I, m, mu, variance, filter_size, epsilon):
@@ -61,7 +104,31 @@ def compute_a(F, I, m, mu, variance, filter_size, epsilon):
 
       @return: image containing a_k for each pixel
     """
-    return
+
+    # init to zeros
+    a = np.zeros(F.shape)
+
+    print("calc a")
+    # run through each pixel
+    for row in range(F.shape[0]):
+
+      # skip edges
+      if (row - filter_size) < 0 or (row + filter_size) > F.shape[0]:
+        continue
+
+      for col in range(F.shape[1]):
+
+        # skip edges
+        if (col - filter_size) < 0 or (col + filter_size) > F.shape[1]:
+          continue
+
+        # the pixel
+        a[row, col] = 1 / (filter_size * filter_size) * np.sum(
+        F[row - filter_size : row + filter_size + 1, col - filter_size : col + filter_size + 1] *
+        I[row - filter_size : row + filter_size + 1, col - filter_size : col + filter_size + 1] -
+        m[row, col] * mu[row, col]) / (variance[row, col] + epsilon)
+
+    return a
 
 
 def compute_b(m, a, mu):
@@ -74,6 +141,10 @@ def compute_b(m, a, mu):
 
       @return: image containing b_k for each pixel
     """
+    print('calc b:')
+    #print('m: ', m)
+    #print('a: ', a)
+    #print('mu: ', mu)
     return m - a * mu
 
 
@@ -82,7 +153,9 @@ def compute_q(mean_a, mean_b, I):
       Compute the final filtered result 'q' as described in the task (equation 6)
       @return: filtered image
     """
-    return
+    print('calc q:')
+
+    return mean_a * I + mean_b
 
 
 def calculate_guided_image_filter(input_img, guidance_img, filter_size, epsilon):
@@ -92,22 +165,27 @@ def calculate_guided_image_filter(input_img, guidance_img, filter_size, epsilon)
 
 def guided_upsampling(input_img, guidance_img, filter_size, epsilon):
 
-    # F is input_img, I is is guidance_img
-
-    # mp is mean of F in wp
-    m = compute_mean(input_img, filter_size)
+    # F is the input_img, I is the grey guidance_img
 
     # compute mean and variance of guidance image
     mu = compute_mean(guidance_img, filter_size)
     variance = compute_variance(guidance_img, filter_size)
 
+    # apply the filter for each channel
+
+    # mp is mean of F in wp
+    m = compute_mean(input_img[:, :, 1], filter_size)
+
     # compute a
-    compute_a(input_img, guidance_img, m, mu, variance, filter_size, epsilon)
+    a = compute_a(input_img[:, :, 1], guidance_img, m, mu, variance, filter_size, epsilon)
 
     # compute b
-    compute_b(m, a, mu)
+    b = compute_b(m, a, mu)
 
-    return
+    # compute Uq
+    Uq = compute_q(np.mean(a), np.mean(b), guidance_img)
+
+    return Uq
 
 
 def prepare_imgs(input_filename, downsample_ratio):
@@ -157,7 +235,7 @@ if __name__ == "__main__":
 
     # Set Parameters
     downsample_ratio = 4
-    filter_size = 3
+    filter_size = 1
     epsilon = 0.1
 
     # Parse Parameter
